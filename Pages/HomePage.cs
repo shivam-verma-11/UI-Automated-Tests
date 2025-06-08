6b3670c0a47f126bb5fdea3c91656c1ab165eba9
Shivam-IITM-DS <24ds1000043@ds.study.iitm.ac.in>
2025-06-08 14:07:18 +0530

    Add project files.

---
+++ Pages/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace GreenCartTests.Pages
+{
+    public class HomePage
+    {
+        private readonly IPage page;
+
+        public HomePage(IPage pages)
+        {
+            page = pages;
+        }
+
+        public async Task RedirectToHomePage(string url)
+        {
+            await page.GotoAsync(url);
+            await page.SetViewportSizeAsync(1280, 720);
+        }
+
+        public async Task<string> GetPageTitle()
+        {
+            return await page.TitleAsync();
+        }
+    }
+}
